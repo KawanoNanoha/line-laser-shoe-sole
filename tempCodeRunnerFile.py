b0c59a59d5d3a4de392613837f983acbf56f553a
KawanoNanoha <k22057nk@apps.kct.ac.jp>
2026-07-03 16:21:52 +0900

    0703角度毎画像保存ファイル追加

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,10 +1,3 @@
-# 中央値から外れた点を除外
-        # y_median = np.median(y)
-        # y_std = np.std(y)
-        # mask_filter = np.abs(y - y_median) < y_std * 1.5
-        # x = x[mask_filter]
-        # y = y[mask_filter]
-
-        # # x範囲が広すぎる断面はスキップ（横線混入）
-        # if x.max() - x.min() > 250:
-        #     continue
+    skip_tilts = [78, 80, 82, 84, 86, 88, 92, 94, 96, 98, 100, 102, 104, 106, 108, 110, 112, 114, 116]
+    if tilt_angle in skip_tilts:
+        continue
